a21e7e73eb6fffa43d36ec0d56fdcb6382b505dd
Joshua Malone <jcharlesmalone@gmail.com>
2020-12-08 21:56:14 -0800

    commit

---
+++ question.js
@@ -0,0 +1,38 @@
+var questions = [
+    {
+      title: "What is javaScript:",
+      choices: ["is a scripting or programming language that allows you to implement complex features on web pages ", "is the standard markup language for Web pages. ", "elements are represented by <> tags."],
+      answer: "is a scripting or programming language that allows you to implement complex features on web pages "
+    },
+    {
+      title: "what is the id symbol?",
+      choices: ["6", "#", "<js>", "<script>"],
+      answer: "#"
+    },
+    {
+      title: "Where should you insert JavaScript?",
+      choices: ["the <head>", "the bottom of the <body>", "anywhere in the HTML", "anywhere in the stylesheet"],
+      answer: "the bottom of the <body>"
+    },
+    {
+      title: "what is a function in JavaScript?",
+      choices: ["how you start a body syntex", " is similar to a procedure—a set of statements that performs a task or calculates a value", "elements are represented by <> tags.", "a command in html"],
+      answer: " is similar to a procedure—a set of statements that performs a task or calculates a value,"
+    },
+    {
+      title: "what does html stand for?",
+      choices: ["Hyper Text Makeup Language", "Hyper Transaction Markup Language", "Hyper Text Markup List", "Hyper Text Markup Language"],
+      answer: "Hyper Text Markup Language"
+    },
+    {
+      title: "what is Jquery?",
+      choices: ["is a lightweight, write less, do more, JavaScript library", "its a html shortcut ", "its a if statement", "its css"],
+      answer: "is a lightweight, write less, do more, JavaScript library"
+    },
+    {
+      title: "what is {} in javascript?",
+      choices: ["numbers between 0 and 10", "is declaring an object. An array has all the features of an object with additional features", "object allows you to perform mathematical tasks on numbers", "means anything that can vary."],
+      answer: "is declaring an object. An array has all the features of an object with additional features"
+    }
+  
+  ]
